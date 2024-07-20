61732d7651a955c1e3fe10df18ef35bd3565b89f
Abhishek Singh <asingh3@icims.com>
2024-07-21 01:06:46 +0530

    added post request for todo

--- demoToDO1/src/main/java/com/example/demoToDO1/TodoController.java
+++ demoToDO1/src/main/java/com/example/demoToDO1/TodoController.java
@@ -2,6 +2,8 @@ package com.example.demoToDO1;
 
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -21,5 +23,10 @@ public class TodoController {
     @GetMapping("/todos")
     public List<Todo> getTodos(){
         return todoList;
+    }
+    @PostMapping("/todos")
+    public  Todo createTodos(@RequestBody Todo newTodo){
+        todoList.add(newTodo);
+        return newTodo;
     }
 }
